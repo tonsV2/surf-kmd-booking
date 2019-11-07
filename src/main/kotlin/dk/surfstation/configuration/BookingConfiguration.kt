7f43eef13189d2213b0e979dfa806e08721b9fde
sj <sebastianthegreatful@gmail.com>
2019-11-07 21:59:27 +0700

    Use correct url

--- src/main/kotlin/dk/surfstation/configuration/BookingConfiguration.kt
+++ src/main/kotlin/dk/surfstation/configuration/BookingConfiguration.kt
@@ -6,8 +6,7 @@ import javax.inject.Singleton
 @Singleton
 class BookingConfiguration {
     companion object {
-        //const val URL = "https://booking-ltk.kmd.dk"
-        const val URL = "https://fritidsportalen-odense.kmd.dk"
+        const val URL = "https://booking-ltk.kmd.dk"
         const val PATH = "/kmd_webapi/api/Monitor/GetFilteredAccessControlRecords?dateTimeFrom={dateTimeFrom}&dateTimeTo={dateTimeTo}&facility=&facilityObject=&partObject=&authenticationCode={authenticationCode}&type=json"
     }
 
